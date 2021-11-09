ab10ad8c6212fb45c734cba54b9583a8380b33e5
Ivan <ivan.ivanov@altius.mk>
2021-11-09 13:27:26 +0100

    #

--- containers/home/Home.js
+++ containers/home/Home.js
@@ -1,33 +1,10 @@
-import { useState, useRef, useEffect } from 'react';
-
-import Home_Beer_Animation from './Home_Beer_Animation';
-import { useRouter } from 'next/router';
-import { useSelector } from 'react-redux';
-import { useWindowSize } from '../../hooks/UseWindowSize';
-
+import Animation from './Animation';
 
 const HomePage = () => {
-
-  const spinner = useSelector(state => state.spinner.is_spinner_loading);
-  const windowSize = useWindowSize();
-  const myRef = useRef();
-  const bottles = {
-    [0]: {
-      url: '/model.glb',
-
-    },
-  
-  };
   return (
-
-    <div className="container-fluid home">
-      <div className="row beer-row align-items-center m-0">
-        <div className="col-12 beer-animate px-0">
-       <div className="w-100 h-100 d-flex mt-5 pt-5" id="beer-1">
-        <Home_Beer_Animation bottle={bottles[0]} />
-          </div>
-    
-        </div>
+    <div className="home">
+      <div className="animation">
+        <Animation />
       </div>
     </div>
   );
