8a8c7c49bc8b6dc7ab50a9c58a072d524d7e0e0b
Ivan <ivan.ivanov@altius.mk>
2021-11-10 14:24:17 +0100

    #

--- containers/home/Animation.js
+++ containers/home/Animation.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState, Suspense } from 'react';
+import { useEffect, useRef, useState, Suspense, useCallback } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import {
   useGLTF,
@@ -9,6 +9,7 @@ import {
   FlyControls,
   MapControls
 } from '@react-three/drei';
+
 import Model from '../../public/Model';
 import Model_1 from '../../public/Model_1';
 import Alternative from '../../public/Alternative';
@@ -18,13 +19,32 @@ import { useSelector, useDispatch } from 'react-redux'
 import { is_Spinner_loading_false } from '../../store/spinner/action'
 
 const Animation = () => {
-  const dispatch = useDispatch()
-  const [ isFinished, setIsFinished ] = useState(false);
-  const spinner = useSelector(state => state.spinner);
-  useEffect(() => {
-   dispatch(is_Spinner_loading_false())
-  }, [Model])
 
+  // const onClick = useCallback(
+  //   e => {
+  //     // filter clicks from dragging
+  //     if (dragDelta.current < 100) {
+  //       const [x, y, z] = rotation.getValue()
+
+  //       setRotation({
+  //         rotation: [x, y + 180, z],
+  //         config: config.default
+  //       })
+  //     }
+  //   },
+  //   [rotation, setRotation]
+  // )
+  // const [props, set] = useSpring(() => ({
+  //   pos: [0, 0, 0],
+  //   scale: [1, 1, 1],
+  //   rotation: [0, 0, 0],
+  //   config: { mass: 10, tension: 1000, friction: 300, precision: 0.00001 }
+  // }))
+
+  // const [{ rotation }, setRotation] = useSpring(() => ({
+  //   rotation: [0, 0, 0],
+  //   config: { mass: 10, tension: 1000, friction: 300, precision: 0.00001 }
+  // }))
 
   
   return (
@@ -41,11 +61,11 @@ const Animation = () => {
       />
      
       <Suspense fallback={null}>
- 
-        <Model  position={[0, -1, 0]} rotationSpeed={1}/>
-        <Digital position={[-5, -3, 0]} />
+  <OrbitControls />
+        <Model  position={[0, -1, 0]} rotationSpeed={1} />
+        {/* <Digital position={[-5, -3, 0]} />
         <Alternative position={[0, -3, 0]} />
-        <Curation position={[+5, -3, 0]} />
+        <Curation position={[+5, -3, 0]} /> */}
       </Suspense>
     </Canvas>
   </>
--- containers/home/Home.js
+++ containers/home/Home.js
@@ -1,20 +1,54 @@
 import Animation from './Animation';
-import { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { is_Spinner_loading_true } from '../../store/spinner/action';
+import digital from '../../styles/assets/images/digital.svg';
+import alternative from '../../styles/assets/images/alternative.svg';
+import curation from '../../styles/assets/images/curation.svg';
+import { motion } from 'framer-motion';
+
+const bounceTransition = {
+  y: {
+    duration: 1,
+    yoyo: Infinity,
+    ease: 'easeOut'
+  },
+
+};
 
 const HomePage = () => {
-  const spinner = useSelector(state => state.spinner);
-  const dispatch = useDispatch();
 
- 
   return (
-    <div className="home">
-    
-        <div className="animation">
-          <Animation  />
-        </div>
-      
+    <div className="container-fluid home d-flex align-items-end">
+      <div className="animation">
+        <Animation />
+      </div>{' '}
+      <div className="pic-row  w-100 d-flex justify-content-around">
+        <motion.div
+          className="digital"
+          transition={bounceTransition}
+          animate={{
+            y: ['10%', '-10%']
+          }}
+        >
+          <img src={digital} alt="" />
+        </motion.div>
+        <motion.div
+          className="alternative"
+          transition={bounceTransition}
+          animate={{
+            y: ['10%', '-10%']
+          }}
+        >
+          <img src={alternative} alt="" />
+        </motion.div>{' '}
+        <motion.div
+          className="curation"
+          transition={bounceTransition}
+          animate={{
+            y: ['10%', '-10%']
+          }}
+        >
+          <img src={curation} alt="" />
+        </motion.div>
+      </div>
     </div>
   );
 };
